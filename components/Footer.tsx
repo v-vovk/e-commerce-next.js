390eacac0331a4835b72f3727d6d22bc35bf920a
Vlad Vovk <wolfykey@gmail.com>
2020-05-30 18:18:10 +0300

    complete

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Next.js app with a <a href="https://snipcart.com">Snipcart</a> - powered
+        store
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link href="/">
+        <img src="/static/logo.svg" alt="" className="header__logo" />
+      </Link>
+      <Link href="/">
+        <h1 className="header__title">FishCastle</h1>
+      </Link>
+      <a
+        className="header__summary snipcart-checkout snipcart-summary"
+        href="#"
+        style={{ textDecoration: "none" }}
+      >
+        <span className="header__price snipcart-total-price"></span>
+      </a>
+    </header>
+  );
+};
+
+export default Header;
